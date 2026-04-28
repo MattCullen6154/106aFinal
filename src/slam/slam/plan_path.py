5b8e3db985b3159b9b907d2957412f60f63e33e9
Matt Cullen <matthew_cullen@berkeley.edu>
2026-04-28 10:54:02 -0700

    debug

--- src/slam/slam/plan_path.py
+++ src/slam/slam/plan_path.py
@@ -1,5 +1,7 @@
 import argparse
 from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+import os
 
 from .planner import astar, build_planner, path_to_world, simplify_path, snap_to_nearest_free, world_to_coarse_cell
 
@@ -10,7 +12,7 @@ def main():
     parser.add_argument("goal", help="Goal waypoint name")
     parser.add_argument(
         "--map-yaml",
-        default=str(Path(__file__).resolve().parent.parent / "maps" / "restaurant_map.yaml"),
+        default=os.path.join(get_package_share_directory('slam'), 'maps', 'restaurant_map.yaml'),#str(Path(__file__).resolve().parent.parent / "maps" / "restaurant_map.yaml"),
         help="Path to the saved ROS map YAML",
     )
     parser.add_argument(
