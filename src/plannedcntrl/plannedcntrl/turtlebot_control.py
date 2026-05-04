e159ba1e6b8db867ad181da3e16faeab408bda9d
Matt Cullen <matthew_cullen@berkeley.edu>
2026-05-04 09:56:49 -0700

    deliveries made

--- src/plannedcntrl/plannedcntrl/turtlebot_control.py
+++ src/plannedcntrl/plannedcntrl/turtlebot_control.py
@@ -18,12 +18,12 @@ class TurtleBotController(Node):
         self.declare_parameter("status_topic", "/nav_status")
         self.declare_parameter("cmd_vel_topic", "/cmd_vel")
         self.declare_parameter("robot_frame", "base_link")
-        self.declare_parameter("position_tolerance", 0.12)
+        self.declare_parameter("position_tolerance", 0.22)
         self.declare_parameter("goal_tolerance", 0.5)
-        self.declare_parameter("linear_gain", 0.7)
-        self.declare_parameter("angular_gain", 0.35)
-        self.declare_parameter("max_linear_speed", 0.3)
-        self.declare_parameter("max_angular_speed", 0.2)
+        self.declare_parameter("linear_gain", 0.25)
+        self.declare_parameter("angular_gain", 0.5)
+        self.declare_parameter("max_linear_speed", 0.4)
+        self.declare_parameter("max_angular_speed", 0.4)
 
         self.path_topic = self.get_parameter("path_topic").value
         self.status_topic = self.get_parameter("status_topic").value
@@ -98,20 +98,13 @@ class TurtleBotController(Node):
         heading_error = self.normalize_angle(heading - robot_yaw)
 
         cmd = Twist()
-        heading_scale = max(0.0, math.cos(heading_error))
 
-        cmd.linear.x = self.clamp(
-            self.linear_gain * distance * heading_scale, 
-            0.0, 
-            self.max_linear_speed,
-        )
-
-        #if abs(heading_error) < 1.0:
-        #    cmd.linear.x = self.clamp(
-        #        self.linear_gain * distance,
-        #        -self.max_linear_speed,
-        #        self.max_linear_speed,
-        #    )
+        if abs(heading_error) < 1.0:
+            cmd.linear.x = self.clamp(
+                self.linear_gain * distance,
+                -self.max_linear_speed,
+                self.max_linear_speed,
+            )
         cmd.angular.z = self.clamp(
             self.angular_gain * heading_error,
             -self.max_angular_speed,
