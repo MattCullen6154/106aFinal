2ce64bb34ab00d24cdd788f87d4a4184c649526d
Matt Cullen <matthew_cullen@berkeley.edu>
2026-05-05 12:17:18 -0700

    .

--- src/plannedcntrl/plannedcntrl/turtlebot_control.py
+++ src/plannedcntrl/plannedcntrl/turtlebot_control.py
@@ -17,7 +17,6 @@ class TurtleBotController(Node):
         self.declare_parameter("path_topic", "/planned_path")
         self.declare_parameter("status_topic", "/nav_status")
         self.declare_parameter("cmd_vel_topic", "/cmd_vel")
-<<<<<<< Updated upstream
         self.declare_parameter("obstacle_hold_topic", "/obstacle_hold")
         self.declare_parameter("robot_frame", "base_link")
         self.declare_parameter("position_tolerance", 0.22)
@@ -26,15 +25,7 @@ class TurtleBotController(Node):
         self.declare_parameter("angular_gain", 0.2)
         self.declare_parameter("max_linear_speed", 0.15)
         self.declare_parameter("max_angular_speed", 0.18)
-=======
-        self.declare_parameter("robot_frame", "base_link")
-        self.declare_parameter("position_tolerance", 0.3)
-        self.declare_parameter("goal_tolerance", 0.25)
-        self.declare_parameter("linear_gain", 0.25)
-        self.declare_parameter("angular_gain", 0.5)
-        self.declare_parameter("max_linear_speed", 0.4)
-        self.declare_parameter("max_angular_speed", 0.4)
->>>>>>> Stashed changes
+
 
         self.path_topic = self.get_parameter("path_topic").value
         self.status_topic = self.get_parameter("status_topic").value
