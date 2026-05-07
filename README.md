ac6c15b6a4885fb0f7b04f339d71e1cc2a748643
Aarsh Shroff <aarsh.shroff@berkeley.edu>
2026-05-07 12:36:16 -0700

    Update instructions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 106aFinal
-Final Project for EECS C106A Sp26
+=== Table ===
+Terminal 1: Backend (Mapping, Planning, Control)
+colcon build
+source install/setup.bash
+ros2 launch waiter backend.launch.py
+
+Terminal 2: Frontend (Waiter CLI)
+colcon build
+source install/setup.bash
+ros2 run waiter waiter_executive
+
+
+=== Kitchen ===
+colcon build
+source install/setup.bash
+export ROS_DOMAIN_ID=[domain id of table]
+ros2 run waiter kitchen_node
--- src/waiter/launch/backend.launch.py
+++ src/waiter/launch/backend.launch.py
@@ -2,36 +2,29 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # 1. Path to the restaurant_mapping launch file
     mapping_pkg_dir = get_package_share_directory("restaurant_mapping")
     mapping_launch_path = os.path.join(mapping_pkg_dir, "launch", "mapping.launch.py")
 
     return LaunchDescription(
         [
-            # 2. Include the Mapping Launch File
+            SetEnvironmentVariable("RCUTILS_COLORIZED_OUTPUT", "1"),
+            SetEnvironmentVariable(
+                "RCUTILS_CONSOLE_OUTPUT_FORMAT", "[{severity}] [{name}]: {message}"
+            ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(mapping_launch_path)
             ),
-            # 3. Run the Control Node (from plannedcntrl package)
             Node(
                 package="plannedcntrl",
                 executable="turtlebot_control",
                 name="turtlebot_control",
                 output="screen",
-            ),
-            # 4. Run the Waiter Executive Node (from waiter package)
-            Node(
-                package="waiter",
-                executable="waiter_executive",
-                name="waiter_executive",
-                output="screen",
-                parameters=[{"use_sim_time": True}],  # Add parameters if needed
             ),
         ]
     )
