0c47b54b57b1e2f83c63bc11c577d379678a720b
Aarsh Shroff <aarsh.shroff@berkeley.edu>
2026-05-07 11:52:06 -0700

    Setup.py update

--- src/waiter/setup.py
+++ src/waiter/setup.py
@@ -1,29 +1,35 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'waiter'
+package_name = "waiter"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ee106a-abn',
-    maintainer_email='somil_03@berkeley.edu',
-    description='Core waiter state machine',
-    license='TODO: License declaration',
+    maintainer="ee106a-abn",
+    maintainer_email="somil_03@berkeley.edu",
+    description="Core waiter state machine",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "waiter_executive = waiter.waiter_executive:main",
             "kitchen_node = waiter.kitchen_node:main",
         ],
